2f23ec973d77ed9baf66db8d46675439304b8ce4
Anastasia <zaytseva7821@ya.ru>
2022-09-10 21:01:51 +0400

    Problem 2

---
+++ Problem 1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+System.Console.WriteLine("Введите число A:");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число B:");
+int B = Convert.ToInt32(Console.ReadLine());
+int AToTheB = A;
+if (B > 0)
+{
+    int count = 1;
+    while (count < B)
+    {
+        AToTheB = AToTheB * A;
+        count++;
+    }
+}
+else
+{
+   
+    System.Console.WriteLine("Введенный вами показатель степени B - ненатуральное число");
+}
+System.Console.WriteLine(AToTheB);
---
+++ Problem 2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+int MakePositive(int arg)
+{
+    return arg = arg *-1;
+}
+
+
+System.Console.WriteLine("Введите ваше число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0)
+num = MakePositive(num);
+int sum = 0;
+while(num>0)
+{
+    sum = sum + num%10;
+    num = num/10;
+}
+System.Console.WriteLine($"Сумма цифр в вашем числе = {sum}.");
