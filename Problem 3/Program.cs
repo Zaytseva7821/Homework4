67cccf3d791bc4f2ed28d3f47ca5ced5df1e9c0b
Anastasia <zaytseva7821@ya.ru>
2022-09-10 21:22:53 +0400

    Problem 3

---
+++ Problem 3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// рандомный массив
+
+int[] array = new int [8];
+System.Console.WriteLine("Ваш массив:");
+for(int i=0; i< array.Length; i++)
+{
+    array[i] = new Random().Next(-100,100);
+    System.Console.Write(array[i]+" ");
+}
+
+// пользовательский
+
+// int[] array = new int [8];
+// for(int i=0; i< array.Length; i++)
+// {
+//     System.Console.WriteLine($"Введите {i+1} число");
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// System.Console.WriteLine("Ваш массив:");
+// for(int i=0; i< array.Length; i++)
+// {
+//     System.Console.Write(array[i]+" ");
+// }
